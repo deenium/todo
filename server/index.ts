eae4a9308ba05a76d6594c8ec9a5e490416caf3b
deenium <nimbekar.darr@gmail.com>
2025-11-03 20:16:09 +0530

    Middleware setup

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,10 @@
-import app from "./server";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors()); // Standard middleware of expressJS application
+app.use(express.json()); // To parse JSON bodies from frontend
 
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
