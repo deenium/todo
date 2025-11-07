afd2186fdcd960530bb8bb4c504ccfac05540dae
deenium <nimbekar.darr@gmail.com>
2025-11-07 22:57:53 +0530

    Add Todo Component

--- client/src/Components/InputTodo.tsx
+++ client/src/Components/InputTodo.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+
 export const InputTodo = () => {
-  return <h1>Input Todo Component</h1>;
+  const [description, setDescription] = useState<string>("");
+
+  const onSubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    // to avoid refreshing the page on submit
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:8000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+  return (
+    <>
+      <h1 className="text-center mt-5">Input Todo Component</h1>
+      <form className="d-flex w-50 mx-auto" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </>
+  );
 };
