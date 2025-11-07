e2b4141707cca6b674072fed457124b3adadfe65
deenium <nimbekar.darr@gmail.com>
2025-11-07 21:00:37 +0530

    CRUD RestAPIs added

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ PERN stack based CRUD application.
 
 For BACKEND: typescript setup (https://www.youtube.com/watch?v=oODlPLfnTIk)
 
-1. move to folder server
+1. move to folder "todo/server"
 2. brew install yarn
 3. yarn init --yes (to initialise repo)
 4. yarn add nodemon typescript @types/express ts-node --dev (to setup dev dependencies for backend)
@@ -14,3 +14,11 @@ For BACKEND: typescript setup (https://www.youtube.com/watch?v=oODlPLfnTIk)
 6. create nodemon.json file (honestly, idk why)
 7. Add scripts to package.json (to setup custom run commands on terminal)
 8. npm install express cors pg (to install expressJS, cors, Postgres)
+
+For DB: (for testing purpose)
+
+1. psql -u postgres (to access your databases)
+2. \l (for seeing list of databases)
+3. \c perntodo (to move to 'perntodo' database, if it exists)
+4. \dt (to see all the tables in the database)
+5. select \* from tableName; (to print all columns and rows of the tableName table)
--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,10 @@ app.use(express.json()); // To parse JSON bodies from frontend
 
 // Routes
 
+// Object type : {id: number, description: string}
+
 // Create a todo
+// POST request to create a new data entry in the database
 app.post("/todos", async (req, res) => {
   try {
     const { description } = req.body;
@@ -18,19 +21,70 @@ app.post("/todos", async (req, res) => {
       "INSERT INTO todo (description) VALUES($1) RETURNING *",
       [description]
     );
-    res.json(newTodo);
+    // "INSERT INTO tableName (column1, column2) VALUES(value1, value2) RETURNING *"
+    res.json(newTodo.rows[0]);
+    // console.log(newTodo);
   } catch (err: any) {
     console.error(err.message);
   }
 });
 
 // get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err: any) {
+    console.error(err.message);
+  }
+});
 
 // get a todo
+// http://localhost:8000/todos/1 to get todo with id=1
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+    // comments : if todo does not exist, no error will be thrown, but no rows will be affected
+  } catch (err: any) {
+    console.log(err.message);
+  }
+});
 
 // update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    // "UPDATE tableName SET column1 = value1, column2 = value2 WHERE condition"
+    res.json("Todo was updated successfully");
+    // comments : if todo does not exist, no error will be thrown, but no rows will be affected
+  } catch (err: any) {
+    console.log(err.message);
+  }
+});
 
 // delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    // "DELETE FROM tableName WHERE condition"
+    res.json("Todo was deleted successfully");
+    // comments : if todo does not exist, no error will be thrown, but no rows will be affected
+  } catch (err: any) {
+    console.log(err.message);
+  }
+});
 
 app.listen(8000, () => {
   console.log("Server is running on http://localhost:8000");
